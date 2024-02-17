cc5dbe5b6f17e6191910cb8574c666b51166a9e5
nitin <santynitin7@gmail.com>
2024-02-17 19:06:25 +0530

    image to image add added with gallery

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { mystore } from "./store/MyStore.js";
+import { mystore } from "./features/myStores.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
