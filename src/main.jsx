e7c7395d1e451b166997fb0b2c758e57673d8a53
nitin <santynitin7@gmail.com>
2024-02-17 19:16:48 +0530

    image to image add added with gallery

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import mystore from "./features/mystore.js";
+import store from "./features/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={mystore}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
